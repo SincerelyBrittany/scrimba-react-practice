59893b70f2a6e0c589cb0885a353d486207f5001
Brittany <sincerelybr@gmail.com>
2021-02-09 19:21:58 -0500

    adds time and date to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,22 @@
 export default function Header() {
+  const date = new Date()
+  const hour = date.getHours()
+
+function getCurrentTime(){
+  if(hour < 13){
+    return "good morning"
+  } else if (hour < 13 && hour > 17){
+    return "good afternoon"
+  } else {
+    return "good evening"
+  }
+}
+
   return (
   <header className="navbar"> 
-  This is the header
+  {/* <h1> {getCurrentTime()} </h1> */}
+        <br />
+    <h2>It is currently {`${date.getHours() % 12}:${date.getMinutes()}`} </h2>
+      <br />
   </header>)
 }
