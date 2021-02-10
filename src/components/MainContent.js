4ed092c59217d98f2c253bf604c1ae1922d44436
Brittany <sincerelybr@gmail.com>
2021-02-09 19:30:16 -0500

    adds inline font styles to main component

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,6 +1,12 @@
 export default function MainContent() {
+  const styles = {
+    color: "#FF8C00", 
+    backgroundColor: "#FF2D00",
+    fontSize: 25
+  }
+
   return (
-  <main className="main-content"> 
+  <main style={styles}> 
   This is the main content
   </main>)
 }
